d2010d79b5db9d2c0dc129b4c1e9d917d6284fb3
Enja Reyes <enja.reyes@gmail.com>
2015-07-18 13:08:07 -0500

    first commit

---
+++ client/app.js
@@ -0,0 +1,21 @@
+// angular.module('app', ['ngRoute'])
+
+// .config(function($routeProvider) {
+//   $routeProvider
+//     .when('/login', {
+//       templateUrl: 'landing.html',
+//       controller: 'loginController'
+//     })    
+//     .when('/signup', {
+//       templateUrl: 'signup.html',
+//       controller: 'loginController'
+//     })
+//     .when('/list', {
+//       templateUrl: 'list.html',
+//       controller: 'ListController'
+//     })
+//     .otherwise({
+//       redirectTo: '/login'
+//     })
+// })
+
---
+++ client/list.js
@@ -0,0 +1,30 @@
+angular.module('app',[])
+
+.controller('listController', function($scope, List){
+  $scope.listItems = List.listItems;
+  angular.extend($scope, List)
+
+  $scope.addItem = function(item){
+    $scope.newToDo = '';
+    List.addItem(item);
+  }
+})
+
+
+.factory('List', function($window){
+  var listItems = [];
+
+  // var getItems = function(){
+    //send request to DB
+  // }
+
+  var addItem = function(item){
+    listItems.push({text:item,done:false});
+  }
+
+  return {
+    // getItems: getItems,
+    listItems: listItems,
+    addItem: addItem  
+  }
+})
