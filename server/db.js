5aaf246ce6af4fecaae0684e27f7cf60445a4731
Enja Reyes <enja.reyes@gmail.com>
2015-08-18 20:03:19 -0500

    clear text input

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 var config = require('../knexfile.js'),
-    env = process.env.NODE_ENV || 'production',
+    env = process.env.NODE_ENV || 'development',
     // env = process.env.NODE_ENV || 'development',
     knex = require('knex')(config[env]);
 
